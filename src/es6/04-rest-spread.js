62d0165f54ca23b3cabf3570c61272e622000594
Daniel <dlozanoc862@gmail.com>
2023-02-02 20:47:46 -0500

    Practice 1, ES6: Spread Operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,3 +10,22 @@ let user = {
 }
 let {username, age} = user;
 console.log(username, user.age);
+
+//Spread Operator
+let person = {
+    name: 'Daniel', 
+    age: 22
+}
+
+let country = 'COL';
+let data = {id: 1, ...person, country}
+console.log(data);
+
+// rest
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4)
