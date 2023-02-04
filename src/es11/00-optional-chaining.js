68d67514830b2b34fd891fa3a76aa417e46602eb
Daniel <dlozanoc862@gmail.com>
2023-02-04 13:18:57 -0500

    Practice 5: ES11: Oprional Chaining

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+const users = {
+    gndx: {
+        country: 'MX'
+    },
+    ana: {
+        country: 'CO'
+    },
+    dani: {
+        country: 'PE'
+    },
+}
+
+console.log(users?.camilo?.country);
